b6e528afe8f65841b44d841186b7cc253053c4b9
Underskinua <zovadim@gmail.com>
2024-01-22 11:20:34 +0200

    gh

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,5 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
 // Change code below this line
