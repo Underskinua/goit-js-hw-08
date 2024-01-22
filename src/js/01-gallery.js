d7bbbae8108cdd1f7a3af3da57d3aa4dc6e6d7ea
Underskinua <zovadim@gmail.com>
2024-01-22 11:35:19 +0200

    return base code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,5 @@
 // Add imports above this line
-
-
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 // Change code below this line
 
-const createGallery = galleryItems =>
-  galleryItems
-    .map(
-      ({ original, preview, description }) =>
-        `<li>
-          <a class="gallery__item" href="${original}">
-            <img style="display:block" class="gallery__image" src="${preview}" alt="${description}" />
-          </a>
-        </li>`
-    )
-    .join('');
-
-const inputGalleryToDocument = (() => {
-  document.querySelector('.gallery').innerHTML = createGallery(galleryItems);
-})();
-
-const gallery = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  scrollZoom: false,
-});
+console.log(galleryItems);
