e7f6ecfd22316ff459e7e2e10e43fd39ae6c0cb3
Underskinua <zovadim@gmail.com>
2024-01-23 12:09:45 +0200

    add import and markup gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+// Generate gallery items markup
+const galleryItemsMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}" data-caption="${description}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      <div class="sl-caption">${description}</div>
+    </a>
+  </li>`
+)
+.join('');
+
+// Add gallery items to the list
+gallery.innerHTML = galleryItemsMarkup;
+
+// Initialize SimpleLightbox
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'self', // Отримати підпис з елементу A-Tag
+  captionType: 'data', // Отримати підпис з атрибута 'data-caption'
+  captionsData: 'caption', // Використовувати значення атрибута 'data-caption' як підпис
+  captionDelay: 250, // Затримка перед появою підпису (в мілісекундах)
+});
+
+
+
+
+
+
+
+//console.log(galleryItems);
