5047d058843d3088d66ea63d37c7d50f3a0854ad
Underskinua <zovadim@gmail.com>
2024-02-04 19:55:38 +0200

    addfix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,29 @@
 import VimeoPlayer from "@vimeo/player";
 import throttle from "lodash.throttle";
 
+// Восстановление воспроизведения
+const localStorageTime = localStorage.getItem("videoplayer-current-time");
+
 const iframe = document.getElementById("vimeo-player");
 const player = new VimeoPlayer(iframe, {
   loop: true,
 });
 
 const throttledTimeupdate = throttle(() => {
-  localStorage.setItem("videoplayer-current-time", player.currentTime);
+const currentTime = player.getCurrentTime();
+  
+  localStorage.setItem("videoplayer-current-time", currentTime);
 }, 1000);
 
 player.on("timeupdate", throttledTimeupdate);
 
-const localStorageTime = localStorage.getItem("videoplayer-current-time");
-
-if (localStorageTime !== null) {
-  player.setCurrentTime(localStorageTime).then(function(seconds) {
+if (localStorageTime !== null & !isNaN(localStorageTime)) {
+  player.setCurrentTime(localStorageTime).then((seconds) => {
     // ...
-  }).catch(function(error) {
+    console.log("Video playback successfully restored to", seconds, "seconds");
+  }).catch((error) => {
     // ...
+    console.error("Error restoring video playback:", error);
   });
 }
 
