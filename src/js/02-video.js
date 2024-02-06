63db04d5ca35a33628900e0faf268ff8e2222208
Underskinua <zovadim@gmail.com>
2024-02-06 18:50:30 +0200

    add on_timeupdate_localStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,26 @@
 import VimeoPlayer from "@vimeo/player";
 import throttle from "lodash.throttle";
 
-// Сохранение ключа для локального хранилища 
-//const localStorageTime = "videoplayer-current-time";
+// Сохранение ключа для локального хранилища --Шаг-5
+const localStorageTime = "videoplayer-current-time";
 
-// Инициалзация плеера
+// Инициалзация плеера ---Шаг-3
 const player = new VimeoPlayer('vimeo-player');
 
-
-
-player.on('play', function() {
+// Функция для обработки события 'play' --Шаг-4
+function onPlay() {
   console.log('played the video!');
-});
-  
+}
 
-// Запуск плеєра
-player.play();
+// Добавление события 'play'
+player.on('play', onPlay);
+
+// Отслеживание события timeupdate --Шаг-4
+player.on('timeupdate', function(data) {
+  // Обновление времени воспроизведения
+  console.log('Відтворено:', data.seconds, 'секунд');
+  console.log('Відсоток:', data.percent);
+  // Сохранение текущего времени в локальном хранилище--Шаг-5
+  localStorage.setItem(localStorageTime, data.seconds);
+  console.log('Я зберігаюсь в localStorageTime');
+});
