620bf46c10a0c012569d3ff153d6923d8e8ebbda
Underskinua <zovadim@gmail.com>
2024-02-25 13:44:50 +0200

    fixedGalleryDescriptionDefault

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,13 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// Описаный в документации
+// Описанный в документации
 import SimpleLightbox from "simplelightbox";
 // Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector('.gallery');
-
+gallery.style.listStyle = 'none'; // Убираем стиль таблицы
 
 // Генерация макета галереи 
 const galleryItemsMarkup = galleryItems
@@ -16,7 +16,7 @@ const galleryItemsMarkup = galleryItems
       `<li class="gallery__item">
       <a class="gallery__link" href="${original}" data-caption="${description}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
-      <div class="sl-caption">${description}</div>
+      
     </a>
   </li>`
 )
@@ -28,10 +28,9 @@ gallery.innerHTML = galleryItemsMarkup;
 // Initialize SimpleLightbox
 new SimpleLightbox('.gallery a', {
   captions: true, // Включаем отображение подписей к изображениям  
-  captionsData: 'alt', // Получить подптсь с атрибута 'alt'
-  
+  captionsData: 'alt', // Получить подптсь с атрибута 'alt'  
   captionDelay: 250, // Задержка перед появлением подписи (в милисекундах)
-  
+    
 });
 
 
